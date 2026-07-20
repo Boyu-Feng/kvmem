c3fa9459d2dbace21be62b03d31aea9b128b6d4f
Boyu-Feng <fengboyu1009@gmail.com>
2026-07-20 14:36:59 +0800

    7.20

--- scripts/paper_figure_style.py
+++ scripts/paper_figure_style.py
@@ -21,10 +21,11 @@ FONT_BAR_VALUE = 17
 SUBPLOTS_LEFT = 0.10
 SUBPLOTS_RIGHT = 0.98
 SUBPLOTS_TOP = 0.88
-LEGEND_BBOX_Y = 1.006
-LEGEND_HANDLELENGTH = 2.0
-LEGEND_HANDLETEXTPAD = 0.6
-LEGEND_COLUMNSPACING = 1.0
+LEGEND_BBOX_Y = 0.992
+LEGEND_HANDLELENGTH = 1.8
+LEGEND_HANDLETEXTPAD = 0.35
+LEGEND_COLUMNSPACING = 0.45
+LEGEND_LABELSPACING = 0.25
 
 
 def panel_height(n_panels: int) -> float:
@@ -54,13 +55,14 @@ def apply_top_legend(
         handlelength=LEGEND_HANDLELENGTH,
         handletextpad=LEGEND_HANDLETEXTPAD,
         columnspacing=LEGEND_COLUMNSPACING,
+        labelspacing=LEGEND_LABELSPACING,
         borderaxespad=0.0,
     )
 
     if ax is not None:
         ax.legend(
             loc="lower center",
-            bbox_to_anchor=(0.5, 1.015),
+            bbox_to_anchor=(0.5, 1.0),
             bbox_transform=ax.transAxes,
             **legend_kwargs,
         )
